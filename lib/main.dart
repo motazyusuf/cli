41178f8983e1b87f7ebe0322453b79ce09932c40
Motaz <motazyusuf@gmail.com>
2025-03-05 12:25:18 +0200

    text changed for the second time

--- lib/main.dart
+++ lib/main.dart
@@ -51,7 +51,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'changed the text',
+              'changed the text 2',
             ),
             Text(
               '$_counter',
